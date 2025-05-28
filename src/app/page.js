cc01d976f9a931e77bb592575f2c59d13369557e
kanhaiya-30 <essitco.reactjs12@gmail.com>
2025-05-28 17:54:19 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import User from './User';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+    
+      
+      <User />
+
+      
+    </div>
+  );
+}
